0960720fdb351262684c86f3d626377b3816a661
Ian An <qan001@uchicago.edu>
2022-04-13 10:35:48 -0500

    Backend Test with Swagger

--- src/main/java/com/example/emos/wx/exception/EmosException.java
+++ src/main/java/com/example/emos/wx/exception/EmosException.java
@@ -3,28 +3,28 @@ package com.example.emos.wx.exception;
 import lombok.Data;
 
 @Data
-public class EmoException extends RuntimeException {
+public class EmosException extends RuntimeException {
 
     private int code = 500;
     private String msg;
 
-    public EmoException(String msg) {
+    public EmosException(String msg) {
         super(msg);
         this.msg = msg;
     }
 
-    public EmoException(String msg, Throwable cause) {
+    public EmosException(String msg, Throwable cause) {
         super(msg, cause);
         this.msg = msg;
     }
 
-    public EmoException(String msg, int code) {
+    public EmosException(String msg, int code) {
         super(msg);
         this.code = code;
         this.msg = msg;
     }
 
-    public EmoException(String msg, int code, Throwable cause) {
+    public EmosException(String msg, int code, Throwable cause) {
         super(msg, cause);
         this.code = code;
         this.msg = msg;

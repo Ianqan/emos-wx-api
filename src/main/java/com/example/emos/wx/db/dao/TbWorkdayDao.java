3124fbb1d4d0faccab8f2482ded1fb8e1ce32275
Ian An <qan001@uchicago.edu>
2022-04-20 17:10:42 -0500

    Validate whether user can log in

--- src/main/java/com/example/emos/wx/db/dao/TbWorkdayDao.java
+++ src/main/java/com/example/emos/wx/db/dao/TbWorkdayDao.java
@@ -1,9 +1,10 @@
 package com.example.emos.wx.db.dao;
 
-import com.example.emos.wx.db.pojo.TbWorkday;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface TbWorkdayDao {
+
+    public Integer searchTodayIsWorkday();
 
 }
